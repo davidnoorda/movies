ffcd5f163b039f2f8001988544e85b2d274e0272
David Noorda <david@macbook-pro.home.gateway>
2016-01-04 00:51:51 +1100

    Some setup changes

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,9 @@ var gulp = require('gulp');
 
 gulp.task('default', function() {
    gulp.src(['./bower_components/bootstrap/dist/css/bootstrap.min.css',
-            './bower_components/angular/angular.min.js'])
+            './bower_components/jquery/dist/jquery.min.js',
+            './bower_components/bootstrap/dist/js/bootstrap.min.js',
+            './bower_components/angular/angular.min.js',
+            './bower_components/angular-route/angular-route.min.js'])
    .pipe(gulp.dest('./app/lib'));
 });
